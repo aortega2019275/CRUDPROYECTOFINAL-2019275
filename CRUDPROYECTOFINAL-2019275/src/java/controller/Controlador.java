2d8cd941ddc6f6d2430a5ddfeaee43aecf823f97
jlopez2016348 <jlopez-2016348@kinal.edu.gt>
2020-09-22 23:36:09 -0600

    se crearon los controladores de eliminar y editar

--- CRUDPROYECTOFINAL-2019275/src/java/controller/Controlador.java
+++ CRUDPROYECTOFINAL-2019275/src/java/controller/Controlador.java
@@ -7,10 +7,13 @@ package controller;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import model.Persona;
+import modelDao.PersonaDAO;
 
 /**
  *
@@ -18,7 +21,12 @@ import javax.servlet.http.HttpServletResponse;
  */
 public class Controlador extends HttpServlet {
 
-   
+    String listar = "view/listar.jsp";
+    String add = "view/add.jsp";
+    String edit = "view/edit.jsp";
+    Persona nuevaPersona = new Persona();
+    PersonaDAO nuevaPersonaDAO = new PersonaDAO();
+    int codigoPersona;
    
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
@@ -49,31 +57,56 @@ public class Controlador extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        
+        String acceso = "";
+        String action = request.getParameter("accion");
+        if (action.equalsIgnoreCase("listar")){
+            acceso = listar;
+        }else if(action.equalsIgnoreCase("add")){
+            acceso = add;
+        }else if(action.equalsIgnoreCase("Agregar")){
+            String DPI = request.getParameter("txtDPI");
+            String nombre = request.getParameter("txtNombre");
+            nuevaPersona.setDPI(DPI);
+            nuevaPersona.setNombrePersona(nombre);
+            nuevaPersonaDAO.add(nuevaPersona);
+            acceso = listar;
+
+        }else if(action.equalsIgnoreCase("editar")){
+            request.setAttribute("codPer", request.getParameter("codigoPersona"));
+            acceso = edit;
+        }else if(action.equalsIgnoreCase("Actualizar")){
+            codigoPersona = Integer.parseInt(request.getParameter("txtCodigoPersona"));
+            String DPI = request.getParameter("txtDPI");
+            String nombres = request.getParameter("txtNombre");
+            nuevaPersona.setCodigoPersona(codigoPersona);
+            nuevaPersona.setDPI(DPI);
+            nuevaPersona.setNombrePersona(nombres);
+            nuevaPersonaDAO.edit(nuevaPersona);
+            acceso=listar;
+        }else if(action.equalsIgnoreCase("eliminar")){
+            codigoPersona = Integer.parseInt(request.getParameter("codigoPersona"));
+            nuevaPersona.setCodigoPersona(codigoPersona);
+            nuevaPersonaDAO.eliminar(codigoPersona);
+            acceso = listar;
+        }
+        
+        
+        RequestDispatcher vista = request.getRequestDispatcher(acceso);
+        
+        vista.forward(request, response);
     }
 
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         processRequest(request, response);
     }
 
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
+
     @Override
     public String getServletInfo() {
         return "Short description";
-    }// </editor-fold>
+    }
 
 }
